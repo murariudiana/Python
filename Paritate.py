2081bddb649688beae64a225fc28c858f08e6ac5
Diana Murariu <50881127+murariudiana@users.noreply.github.com>
2021-02-09 22:25:20 +0200

    Add files via upload

---
+++ Paritate.py
@@ -0,0 +1,15 @@
+def paritate(x):
+ print("RULEZ FUNCTIA")
+
+ if x % 2 == 0 :
+   print("Numarul este par")
+ else:
+   print("Numarul este impar")
+ 
+if __name__ == "__main__":
+    x =int(input("Introduceti numarul: "))
+    paritate(x)
+
+
+
+
